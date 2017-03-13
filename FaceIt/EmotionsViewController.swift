8789cbf5c9f6d3ef8182f036f9872834a10e58d4
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-03-13 16:41:24 +0200

    Added EmotionsView and segues

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by ooras on 13/03/2017.
+//  Copyright © 2017 oporas. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationViewController = segue.destination
+        if let navigationController = destinationViewController as? UINavigationController {
+            destinationViewController = navigationController.visibleViewController ?? destinationViewController
+        }
+        if let faceViewController = destinationViewController as? FaceViewController,
+            let indentifier = segue.identifier,
+            let expression = emotionalFaces[indentifier] {
+                faceViewController.expression = expression
+            faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
+        }
+    }
+
+    private let emotionalFaces: Dictionary<String,FacialExpression> = [
+        "sad" : FacialExpression(eyes: .closed, mouth: .frown),
+        "happy" : FacialExpression(eyes: .open, mouth: .smile),
+        "worried" : FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+
+}
